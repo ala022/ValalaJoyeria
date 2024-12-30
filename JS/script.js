68f1be06c8a5d511a62def038f1a9bd4dd97cb4f
ala022 <durandangelucero@gmail.com>
2024-12-30 15:25:42 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -28,11 +28,6 @@ function inicializarAplicacion() {
     if (emptyCartButton) {
         emptyCartButton.addEventListener("click", vaciarCarrito);
     }
-    // Verificar si el botón de vaciar carrito existe antes de agregar el eventListener
-    const emptyCartButton = document.getElementById("empty-cart");
-    if (emptyCartButton) {
-        emptyCartButton.addEventListener("click", vaciarCarrito);
-    }
 }
 
 // Cargar productos desde un JSON
